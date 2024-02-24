20ad04b04b0f57fb4ab7ee7eaab9e3f01a40d948
Julian Koripamo <jkoripamo@gmail.com>
2024-02-24 20:09:29 +0100

    team model

---
+++ lib/models/team.dart
@@ -0,0 +1,11 @@
+class Team {
+  final String fullName;
+  final String abbreviation;
+  final String city;
+
+  Team({
+    required this.fullName,
+    required this.abbreviation,
+    required this.city,
+  });
+}
