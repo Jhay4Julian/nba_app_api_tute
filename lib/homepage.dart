e75c11d9668b6084e3318bfdaad07bd5e54fdfd2
Julian Koripamo <jkoripamo@gmail.com>
2024-02-24 20:11:57 +0100

    api call and ui

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,10 +1,77 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
-class HomePage extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:nba_app_api_tute/models/team.dart';
+
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<Team> teams = [];
+
+  // get teams
+  Future getTeams() async {
+    var response = await http.get(Uri.https('api.balldontlie.io', 'v1/teams'),
+        headers: {'Authorization': '047a664a-0234-41e4-abc4-d8cc40356e9f'});
+    var jsonData = jsonDecode(response.body);
+
+    for (var eachTeam in jsonData['data']) {
+      final team = Team(
+        fullName: eachTeam['full_name'],
+        abbreviation: eachTeam['abbreviation'],
+        city: eachTeam['city'],
+      );
+      teams.add(team);
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    getTeams();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'NBA Teams',
+          style: TextStyle(color: Colors.white),
+        ),
+        centerTitle: true,
+        backgroundColor: Theme.of(context).colorScheme.primary,
+      ),
+      body: FutureBuilder(
+          future: getTeams(),
+          builder: (context, snapshot) {
+            // is it done loading? show team data
+            if (snapshot.connectionState == ConnectionState.done) {
+              return ListView.builder(
+                  itemCount: teams.length,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        decoration: const BoxDecoration(
+                            color: Color.fromARGB(255, 243, 205, 250),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(20))),
+                        child: ListTile(
+                          title: Text(teams[index].abbreviation),
+                          subtitle: Text(teams[index].fullName),
+                        ),
+                      ),
+                    );
+                  });
+            }
+            // if its still loading, show loading circle
+            else {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+          }),
+    );
   }
 }
